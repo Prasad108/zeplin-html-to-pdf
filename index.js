f87b58d8c25beafc78892ef49710ecf2a8c354c3
Prasad Ashok Dukale <prasad.dukale@atos.net>
2020-09-09 18:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ exports.handler = function handler(event, context, callback) {
         orientation : event.orientation || 'Landscape'
     }
 
-    wkhtmltopdf(event.html)
+    wkhtmltopdf(event.html, wkhtmltopdfOptions )
         .then(buffer => {
         console.log("converted the html to PDF");
         S3.putObject({

200076c445f93a2458508e2e748f5d68b6aaa013
Prasad Ashok Dukale <prasad.dukale@atos.net>
2020-09-10 16:47:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const errorUtil = require("./utils/error");
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 const fs = require('fs');
+createWriteStream = require('fs').createWriteStream;
 
 
 exports.handler = function(event, context, callback) {
@@ -12,17 +13,26 @@ exports.handler = function(event, context, callback) {
         pageSize : event.pagesize || 'a4',
         orientation : event.orientation || 'Landscape'
     }
+	
+	
 
     var  content = event.url;
-	wkhtmltopdf(content, {}, function(error, stream) {
-		if ( error ) {
-			console.error('wkhtmltopdf failed!');
-			callback(error);
-			return;
-		}
+	
+	wkhtmltopdf('http://google.com/', 'letter')
+    	.pipe(createWriteStream('demo1.pdf'));
+	return { "status" : "success"
+	}
+	
+	
+// 	wkhtmltopdf(content, {}, function(error, stream) {
+// 		if ( error ) {
+// 			console.error('wkhtmltopdf failed!');
+// 			callback(error);
+// 			return;
+// 		}
 		
-	console.log('PDF generation was successful. Starting S3 upload...');
-		callback(null, 'Success');
+// 	console.log('PDF generation was successful. Starting S3 upload...');
+// 		callback(null, 'Success');
 	});
 // 	wkhtmltopdf(content, wkhtmltopdfOptions)
 // 	.then(buffer => {

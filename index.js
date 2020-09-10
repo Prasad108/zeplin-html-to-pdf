5602969efc18493305dd5ac133572c3188446e94
Prasad Ashok Dukale <prasad.dukale@atos.net>
2020-09-10 14:10:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const fs = require('fs');
 
 exports.handler = function(event, context, callback) {
 	console.log(event);
-    var body = JSON.parse(event.body);
-    var content;
-	if (body.htmlBase64 != null) {
-        content = new Buffer(body.htmlBase64, 'base64').toString('utf8');
-    } else {
-	    content = body.url;
-    }
+    var  content = body.url;
 	wkhtmltopdf(content, body.options, function(code, signal) {
 	    const response = {
             statusCode: 200,

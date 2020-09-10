ee991c2e002b04729866d0613cc485f3f463c83d
Prasad Ashok Dukale <prasad.dukale@atos.net>
2020-09-10 14:05:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const fs = require('fs');
 exports.handler = function(event, context, callback) {
 	console.log(event);
     var body = JSON.parse(event.body);
-	var memStream = new MemoryStream();
     var content;
 	if (body.htmlBase64 != null) {
         content = new Buffer(body.htmlBase64, 'base64').toString('utf8');

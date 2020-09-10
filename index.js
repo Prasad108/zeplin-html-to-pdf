3a1ab1ebd1ad6e3b1cdf9b924e12bef572396985
Prasad Ashok Dukale <prasad.dukale@atos.net>
2020-09-10 16:49:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ exports.handler = function(event, context, callback) {
     var  content = event.url;
 	
 	wkhtmltopdf('http://google.com/', 'letter')
-    	.pipe(createWriteStream('demo1.pdf'));
+    	.pipe(createWriteStream('demo1.pdf');
 	return { "status" : "success"
 	}
 	
